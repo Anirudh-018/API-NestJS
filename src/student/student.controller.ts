293ac120c9c41a17b66f6250a6934ae67918c89c
Anirudh r k <menonani7@gmail.com>
2023-03-15 15:01:13 +0530

    status codes done

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -8,27 +8,31 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 @Controller('student')
 export class StudentController {
     constructor(private studentService:StudentService){}
+
     @Post()
     createStudent(@Body() createStudent:CreateStudentDto,
     @Res() res: Response,){
         if(Object.keys(createStudent).length>0){
             const stud=this.studentService.createStudent(createStudent);
-            const status=HttpStatus.OK+" created";
+            const status=HttpStatus.CREATED+" Created";
             res.send({status,stud})
         }
         else{
             res.status(400).send(HttpStatus.BAD_REQUEST+" Bad request")
         }
     }
+
     @Get(':id')
     async fetchStudents(
         @Param('id',ParseIntPipe) id:number,
         @Req() req:Request,
         @Res() res: Response,
-        ){
+        )
+        {
             const students=await this.studentService.fetchStudents({id});
             if(students){
-                res.status(200).send("created")
+                const status=HttpStatus.OK+" found";
+                res.send({status,students})
             }
             else{
                 throw new HttpException("empty",HttpStatus.BAD_REQUEST);
@@ -38,14 +42,25 @@ export class StudentController {
     fetchAllStudents(){
         return this.studentService.fetchAllStudents();
     }
-        @Patch(':id')
+
+    @Patch(':id')
     async updateStudentById(@Param('id',ParseIntPipe) id:number,
-    @Body() updateStudentDto:UpdateStudentDto){
-        await this.studentService.updateStudent(id,updateStudentDto);
-    }
-    @Delete(':id')
-    async deleteStudentById(@Param('id',ParseIntPipe) id:number){
-        this.studentService.deleteStudent(id)
+    @Body() updateStudentDto:UpdateStudentDto,@Res() res:Response){
+        const stud=await this.studentService.updateStudent(id,updateStudentDto);
+        const status=HttpStatus.OK+" updated";
+        res.send({status,stud});
+        return stud;
     }
 
+    @Delete(':id')
+    async deleteStudentById(@Param('id',ParseIntPipe) id:number,@Res() res:Response){
+        const deleteResponse=await this.studentService.deleteStudent(id)
+        if(deleteResponse){
+            const status=HttpStatus.NO_CONTENT+" no content";
+            res.send({status,deleteResponse})
+        }
+        else{
+            res.send(HttpStatus.NOT_FOUND+" not found");
+        }
+    }
 }
